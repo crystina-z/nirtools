c80c6cd5874a947c58055cc13684d068f17afb5d
Crytina <xzhangbx@ust.hk>
2020-06-18 22:01:11 +0000

    add hugging face inspector

--- src/tensorflow/ckpt.py
+++ src/tensorflow/ckpt.py
@@ -4,6 +4,11 @@ def inspect(ckpt_path, tensor_name=""):
     print_tensors_in_checkpoint_file(file_name=ckpt_path, tensor_name=tensor_name, all_tensors=False)
 
 
+def inspect_huggingface_model(HFClass, bert_model="bert-base-uncased"):
+    model = HFClass.from_pretrained(bert_model)
+    return model.trainable_weight
+
+
 def rename_variables(tvars, patterns, prefix="", save=False, outp_fn="converted.ckpt"):
     """
         This function need to be invoked outside of tf.Session()
@@ -28,6 +33,5 @@ def rename_variables(tvars, patterns, prefix="", save=False, outp_fn="converted.
             sess.run(tf.global_variables_initializer())
             saver.save(sess, outp_fn)
     return renamed_tvars 
-
 
 
