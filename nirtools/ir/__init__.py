6add553c242a4085fbfa5d120b9d0cc35dac9b5d
Crystina333 <xzhangbx@connect.ust.hk>
2020-07-01 19:08:30 +0800

    add docstring to all functions

--- nirtools/ir/__init__.py
+++ nirtools/ir/__init__.py
@@ -2,6 +2,12 @@ from collections import defaultdict
 
 
 def load_qrels(fn):
+    """
+    Loading trec format query relevance file into a dictionary
+
+    :param fn: qrel file path
+    :return: dict, in format {qid: {docid: label, ...}, ...}
+    """
     qrels = defaultdict(dict)
     with open(fn, "r", encoding="utf-8") as f:
         for line in fn:
@@ -11,6 +17,12 @@ def load_qrels(fn):
 
 
 def load_runfile(fn):
+    """
+    Loading trec format runfile into a dictionary
+
+    :param fn: runfile path
+    :return: dict, in format {qid: {docid: score, ...}, ...}
+    """
     runs = defaultdict(dict)
     with open(fn, "r", encoding="utf-8") as f:
         for line in fn:
--- nirtools/tensorflow/ckpt.py
+++ nirtools/tensorflow/ckpt.py
@@ -4,25 +4,46 @@ from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoi
 
 
 def inspect(ckpt_path, tensor_name=""):
+    """
+    read the tensorflow checkpoint file and print out the name and shape of all the tensors
+    or the one given
+
+    :param ckpt_path: tensorflow checkpoint file path
+    :param tensor_name: the name of desired tensor, default to be ""
+    :return:
+    """
     print_tensors_in_checkpoint_file(
         file_name=ckpt_path, tensor_name=tensor_name, all_tensors=False
     )
 
 
 def inspect_huggingface_model(HFClass, bert_model="bert-base-uncased"):
+    """
+    fetch all trainable tensors from the given huggingface tensorflow model
+
+    :param HFClass: The model class
+    :param bert_model:
+    :return: all trainable variables
+    """
     model = HFClass.from_pretrained(bert_model)
     return model.trainable_weight
 
 
 def rename_variables(tvars, patterns, prefix="", save=False, outp_fn="converted.ckpt"):
     """
-        This function need to be invoked outside of tf.Session()
-        tvars: tf.trainable_variables, 
-        pattern: OrderedDict, {pattern: replacement}, pattern appears later has higher priority
+    rename the given tensorflow trainable variables by replacing the keys in patterns with the values,
+    and prepend the prefix
 
-    return a list of renamed variables 
+    This function need to be invoked inside of tf.Session()
+
+    :param tvars: tf.trainable_variables
+    :param patterns: OrderedDict, {pattern: replacement}, pattern appears later has higher priority
+    :param prefix: prefix to be appended before all the tensors' name
+    :param save: whether to save the converted tensor into dist, if True, the outp_fn must be a valid path name
+    :param outp_fn: the path to save the converted tensors, would be ignored if `save` is set to False,
+        default "converted.ckpt"
+    :return: a list of renamed variables
     """
-
     def convert(name):
         for ori, tgt in patterns.items():
             name = name.replace(ori, tgt)
@@ -42,6 +63,13 @@ def rename_variables(tvars, patterns, prefix="", save=False, outp_fn="converted.
 
 
 def convert_tvars_to_dict(tvars, name2nparr=None):
+    """
+    convert the input training variables to a dictionary
+
+    :param tvars: the tensorflow trainable variables
+    :param name2nparr: a dictionary in same format with output, if given, the converted tvars will be added to this
+    :return: the dictionary in the format of {dict: np.Array}
+    """
     if not name2nparr:
         name2nparr = {}
 
@@ -50,4 +78,10 @@ def convert_tvars_to_dict(tvars, name2nparr=None):
 
 
 def convert_ckpt_file_to_dict(path):
+    """
+    load the tensorflow dictionary from checkpoint file and convert it to a dictionary mapping name to value
+
+    :param path: the checkpoint file path
+    :return: the dictionary in the format of {dict: np.Array}
+    """
     return {name: load_variable(ckpt_dir_or_file=path, name=name) for name, shape in list_variables(path)}
