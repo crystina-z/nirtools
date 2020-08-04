b6d20aeae23d39d37ba0b857fd2bf5bf3e414179
Crystina333 <xzhangbx@connect.ust.hk>
2020-08-04 17:04:48 +0800

    add fn to write qrel and runs

--- nirtools/ir/__init__.py
+++ nirtools/ir/__init__.py
@@ -1,4 +1,14 @@
-from collections import defaultdict
+import os
+from collections import defaultdict, OrderedDict
+
+
+def sort_qid_docid_value_dict(d):
+    sorted_d = OrderedDict()
+    qids = sorted(d.keys(), key=lambda k: int(k))  # sort according to qid int value rather than string value
+    for qid in qids:  # sort according to label/score, from large to small
+        docs = sorted(d[qid].items(), key=lambda kv: kv[1], reverse=True)
+        sorted_d[qid] = {k: v for k, v in docs}
+    return sorted_d
 
 
 def load_qrels(fn):
@@ -29,6 +39,25 @@ def load_runs(fn):
             qid, _, docid, _, score, _ = line.strip().split()
             runs[qid][docid] = float(score)
     return runs
+
+
+def write_qrels(qrels_dict, outp_fn):
+    os.makedirs(os.path.dirname(outp_fn), exist_ok=True)
+    sorted_qrels = sort_qid_docid_value_dict(qrels_dict)
+    with open(outp_fn, "w", encoding="utf-8") as f:
+        for qid in sorted_qrels:
+            for docid, value in sorted_qrels[qid].items():
+                f.write(f"{qid}\tQ0\t{docid}\t{value}\n")
+
+
+def write_runs(run_dict, outp_fn, label="test"):
+    os.makedirs(os.path.dirname(outp_fn), exist_ok=True)
+    sorted_run_dict = sort_qid_docid_value_dict(run_dict)
+    with open(outp_fn, "w", encoding="utf-8") as f:
+        for qid in sorted_run_dict:
+            for rank, (docid, score) in enumerate(sorted_run_dict[qid].items()):
+                f.write(f"{qid}\tQ0\t{docid}\t{rank+1}\t{score}\t{label}\n")
+                # e.g. 1 Q0 DOC_1 1 -0.1 test
 
 
 def load_topic_trec(topic_fn, field="title"):
